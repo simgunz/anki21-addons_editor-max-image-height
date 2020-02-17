26c45c74f26e9e717934f884985657cfe8cd41e8
AnKingMed <57678116+AnKingMed@users.noreply.github.com>
2020-02-17 13:01:10 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -21,8 +21,13 @@ from aqt.editor import Editor
 
 def setBrowserMaxImageHeight(self):
     config = self.mw.addonManager.getConfig(__name__)
-    self.web.eval('''$('head').append('<style type="text/css">'''
-                  '''#fields img{{ max-height: {height};}}</style>')'''
-                  .format(height=config['max-height']))
+    if config['height_or_width'] == "height":
+        self.web.eval('''$('head').append('<style type="text/css">'''
+                      '''#fields img{{ max-height: {height};}}</style>')'''
+                      .format(height=config['max-height']))
+    elif config['height_or_width'] == "width":
+        self.web.eval('''$('head').append('<style type="text/css">'''
+                      '''#fields img{{ max-width: {width};}}</style>')'''
+                      .format(width=config['max-width']))
 
 Editor.setupWeb = wrap(Editor.setupWeb, setBrowserMaxImageHeight)
